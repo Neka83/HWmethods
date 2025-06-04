195baa27b24e3dfa9b73cde3f188e8904ffa30cb
Neka83 <n_neike@mail.ru>
2025-06-04 07:54:03 +0500

    HW methods #1

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,27 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("// Task 1");
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
 
+        System.out.println("Ф.И.О сотрудника - " + fullName);
+        System.out.println();
+        System.out.println("// Task 2");
 
+        String fullNameOfEmployee = "Ivanov Ivan Ivanovich";
+        String reportFullName = fullNameOfEmployee.toUpperCase();
 
+        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета - " + reportFullName);
+        System.out.println();
+        System.out.println("// Task 3");
+
+        String employeeFullName = "Иванов Семён Семёнович";
+        String correctedFullName = employeeFullName.replace("ё", "e");
+
+        System.out.println("Данные Ф.И.О сотрудника  - " + correctedFullName);
     }
 }
